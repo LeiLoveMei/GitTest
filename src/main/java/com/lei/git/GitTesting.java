424a8eb3a04d65df5258671bc1c9099b77de0a1e
lei <leiandmei_glb@outlook.com>
2021-05-12 23:14:37 +0800

    first commit

---
+++ src/main/java/com/lei/git/GitTesting.java
@@ -0,0 +1,11 @@
+package com.lei.git;
+
+/**
+ * @author Lei
+ * @date 2021/5/12
+ */
+public class GitTesting {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
